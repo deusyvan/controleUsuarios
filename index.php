03f529d2db2a3d5c514ed764e1251d3b8f13bfd4
root <root@cciexinfor23.sef.eb.mil.br>
2018-05-22 11:19:33 -0300

    Tabela Simples

---
+++ index.php
@@ -0,0 +1,19 @@
+<table border="0" width="100%">
+  <tr>
+    <th>Nome</th>
+    <th>E-mail</th>
+    <th>Ações</th>
+  </tr>
+  <tr>
+    <td>Row 1: Col 1</td>
+    <td>Row 1: Col 2</td>
+    <td>Row 1: Col 3</td>
+  </tr>
+</table>
+
+
+
+
+<?php
+
+?>
