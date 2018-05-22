019d3ffdd90227c50020aba49cc8afd8ddb353e0
root <root@cciexinfor23.sef.eb.mil.br>
2018-05-22 11:26:42 -0300

    Importando a configuração e fazendo consulta

--- index.php
+++ index.php
@@ -1,19 +1,25 @@
+<?php 
+    require 'config.php';
+?>
+
 <table border="0" width="100%">
   <tr>
     <th>Nome</th>
     <th>E-mail</th>
     <th>Ações</th>
   </tr>
+  
+  <?php 
+    $sql = "SELECT * FROM usuarios";
+    $sql = $pdo->query($sql);
+    if($sql->rowCount() > 0 ){
+        echo 'Ta tudo bem.';
+    }
+  
+  ?>
   <tr>
     <td>Row 1: Col 1</td>
     <td>Row 1: Col 2</td>
     <td>Row 1: Col 3</td>
   </tr>
 </table>
-
-
-
-
-<?php
-
-?>
