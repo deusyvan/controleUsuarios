8998eaff148fc6ee7438693315a6ddee86b119e6
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-05-22 16:46:15 -0300

    Excluindo o usuário

--- excluir.php
+++ excluir.php
@@ -3,6 +3,13 @@ require 'config.php';
 
 if(isset($_GET['id']) && empty($_GET['id']) == FALSE){
     
+    $id = addslashes($_GET['id']);
+    
+    $sql = "DELETE FROM usuarios WHERE id = '$id'";
+    $pdo->query($sql);
+    
+    header("Location: index.php");
+    
 } else {
     header("Location: index.php");
 }
