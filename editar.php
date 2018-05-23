89337f6b2f34f2adc4f6ae732966715b64bd2e57
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-05-23 09:45:51 -0300

    Formulário para editar

--- editar.php
+++ editar.php
@@ -14,4 +14,16 @@
  
  ?>
  
- 
+ <form method="POST">
+ 	Nome: <br/>
+	<input type="text" name="nome" value=""/><br/><br/>
+	
+	E-mail: <br/>
+	<input type="text" name="email" value=""/><br/><br/>
+	
+	Senha: <br/>
+	<input type="password" name="senha" value=""/><br/><br/>
+	
+	<input type="submit" value="Atualizar"/>
+ 	
+ </form>
