c09a5c1b93971b28e0594f4bfd9db5fdc175b36f
2º Sgt Deusyvan <deusyvan.silva@eb.mil.br>
2018-05-23 09:43:36 -0300

    Proteção para receber o id

--- editar.php
+++ editar.php
@@ -5,9 +5,13 @@
  
  if(isset($_GET['id']) && empty($_GET['id']) == FALSE){
      
-     
+     $id = addslashes($_GET['id']);
      
      
  } else {
      header("Location: index.php");
  }
+ 
+ ?>
+ 
+ 
